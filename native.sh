d95bc77065f8fce711b6df0794573f12940ecc55
Luís Serralheiro <encavadas@gmail.com>
2024-04-01 10:30:44 +0100

    Bumping apized

---
+++ native.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+VERSION=$(cat gradle.properties | grep version | awk -F '=' '{ print $2 }')
+./gradlew clean server:dockerBuildNative && \
+  docker rm -f auth && \
+  docker run -d \
+  --name auth \
+  --restart=unless-stopped \
+  -p 8066:80 \
+  -e DB_HOST=host.docker.internal \
+  -e MANAGEMENT_PORT=80 \
+  -e AUTH_TOKEN_DOMAIN=mooglest.com \
+  -e RABBITMQ_URI=amqp://host.docker.internal \
+  -e RABBITMQ_EXCHANGE=srt \
+  "auth:$VERSION"
