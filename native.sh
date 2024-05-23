b83a098f154decca7166b3eecc22ad201c604526
Luís Serralheiro <encavadas@gmail.com>
2024-05-23 21:41:47 +0100

    Bumping apized

--- native.sh
+++ native.sh
@@ -8,7 +8,7 @@ VERSION=$(cat gradle.properties | grep version | awk -F '=' '{ print $2 }')
   -p 8066:80 \
   -e DB_HOST=host.docker.internal \
   -e MANAGEMENT_PORT=80 \
-  -e AUTH_TOKEN_DOMAIN=mooglest.com \
+  -e AUTH_TOKEN_DOMAIN=local.mooglest.com \
   -e RABBITMQ_URI=amqp://host.docker.internal \
   -e RABBITMQ_EXCHANGE=srt \
   "auth:$VERSION"
